47d3c5b3a8471daaf78b1dd754d2afd6a582ee37
Abdalla Diaai <abdalla.diaai@outlook.com>
2025-01-18 10:05:16 +0000

    added beginning of order agent.

--- python/api/agents/order_agent.py
+++ python/api/agents/order_agent.py
@@ -67,9 +67,16 @@ class OrderAgent:
             Your output should be in a structured json format like so. Each key is a string and each value is a string. Make sure to follow the format exactly:
             {
             "chain of thought": Write down your critical thinking about what is the maximum task number the user is on write now. Then write down your critical thinking about the user input and it's relation to the coffee shop process. Then write down your thinking about how you should respond in the response parameter taking into consideration the Things to NOT DO section. and Focus on the things that you should not do. 
-            "step number": Determine which task you are on based on the conversation.
+            "step number": determine which task you are on based on the conversation.
             "order": this is going to be a list of jsons like so. [{"item": put the item name, "quanitity": put the number that the user wants from this item, "price": put the total price of the item }]
             "response": write the a response to the user
             }
             
             """
+        
+        last_order_taken_status = ""
+        for i in range(len(messages) - 1, 0, -1):
+            message = messages[i]
+            agent_name = message.get("memory", {}).get("agent", "")
+            if message['role'] == "assistant" and agent_name == "order_agent":
+                step_number = message['memory']['step number']
