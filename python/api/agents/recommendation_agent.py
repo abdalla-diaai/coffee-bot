9c97e6a3e92f0682d27fd5238700680df522c7e4
Abdalla Diaai <abdalla.diaai@outlook.com>
2025-01-18 00:43:14 +0000

    finalised recommendation agent.

--- python/api/agents/recommendation_agent.py
+++ python/api/agents/recommendation_agent.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import pandas as pd
 from openai import OpenAI
 import os
+from .utils import get_chatbot_response
 
 load_dotenv()
 
@@ -19,10 +20,10 @@ class RecommendationAgent:
             self.apriori_recommendations = json.load(file)
 
         self.popular_recommendations = pd.read_csv(popular_recommendation_path)
-        self.products = self.popular_recommendations["product"].tolist()
-        self.product_categories = self.popular_recommendations[
-            "product_category"
-        ].tolist()
+        self.products = self.popular_recommendations['product'].tolist()
+        self.product_categories = list(set(self.popular_recommendations[
+            'product_category'
+        ].tolist()))
 
     def get_apriori_recommendation(self, products, top_k=5):
         recommendation_list = []
@@ -32,7 +33,7 @@ class RecommendationAgent:
 
         # sort recommendation list by confidence
         recommendation_list = sorted(
-            recommendation_list, key=lambda x: x["confidence"], reverse=True
+            recommendation_list, key=lambda x: x['confidence'], reverse=True
         )
 
         # limit recommendation to 2 per category
@@ -41,13 +42,13 @@ class RecommendationAgent:
         for recommendation in recommendation_list:
             if recommendation in recommendations:
                 continue
-            product_category = recommendation["product_category"]
+            product_category = recommendation['product_category']
             if product_category not in recommendation_per_category:
                 recommendation_per_category[product_category] = 0
             if recommendation_per_category[product_category] >= 2:
                 continue
             recommendation_per_category[product_category] += 1
-            recommendations.append(recommendation["product"])
+            recommendations.append(recommendation['product'])
 
             if len(recommendations) >= top_k:
                 return recommendations
@@ -61,7 +62,7 @@ class RecommendationAgent:
 
         if product_categories is not None:
             recommendations_df = self.popular_recommendations[
-                self.popular_recommendations["product_category"].isin(
+                self.popular_recommendations['product_category'].isin(
                     product_categories
                 )
             ]
@@ -72,4 +73,120 @@ class RecommendationAgent:
 
         if recommendations_df.shape[0] == 0:
             return []
-        return recommendations_df["product"].tolist()[:top_k]
+        return recommendations_df['product'].tolist()[:top_k]
+
+    def recommendation_classification(self, messages):
+        system_prompt = """ You are a helpful AI assistant for a coffee shop application which serves drinks and pastries. We have 3 types of recommendations:
+
+        1. Apriori Recommendations: These are recommendations based on the user's order history. We recommend items that are frequently bought together with the items in the user's order.
+        2. Popular Recommendations: These are recommendations based on the popularity of items in the coffee shop. We recommend items that are popular among customers.
+        3. Popular Recommendations by Category: Here the user asks to recommend them product in a category. Like what coffee do you recommend me to get?. We recommend items that are popular in the category of the user's requested category.
+        
+        Here is the list of items in the coffee shop:
+        """+ ",".join(self.products) + """
+        Here is the list of Categories we have in the coffee shop:
+        """ + ",".join(self.product_categories) + """
+
+        Your task is to determine which type of recommendation to provide based on the user's message.
+
+        Your output should be in a structured json format like so. Each key is a string and each value is a string. Make sure to follow the format exactly:
+        {
+        "chain of thought": Write down your critical thinking about what type of recommendation is this input relevant to.
+        "recommendation_type": "apriori" or "popular" or "popular by category". Pick one of those and only write the word.
+        "parameters": This is a python list. It's either a list of of items for apriori recommendations or a list of categories for popular by category recommendations. Leave it empty for popular recommendations. Make sure to use the exact strings from the list of items and categories above.
+        }
+        """
+        input_messages = [{'role': 'system', 'content': system_prompt}] + messages[-3:]
+
+        chatbot_output = get_chatbot_response(self.client, self.model_name, input_messages)
+        output = self.postprocess_classification(chatbot_output)
+
+        return output
+    
+    def postprocess_classification(self, output):
+        output = json.loads(output)
+
+        dict_output = {
+            'recommendation_type': output['recommendation_type'],
+            'parameters': output['parameters'],
+        }
+        return dict_output
+    
+
+    def get_recommendation_from_order(self, messages, order):
+        products = []
+        for product in order:
+            products.append(product['item'])
+        
+        recommendations = self.apriori_recommendations(products = products)
+        recommendations_str = ", ".join(recommendations)
+        system_prompt = f"""
+        You are a helpful AI assistant for a coffee shop application which serves drinks and pastries.
+        your task is to recommend items to the user based on their order.
+
+        I will provide what items you should recommend to the user based on their order in the user message. 
+        """
+
+        prompt = f"""
+        {messages[-1]['content']}
+
+        Please recommend me those items exactly: {recommendations_str}
+        """
+
+        messages[-1]['content'] = prompt
+        input_messages = [{'role': 'system', 'content': system_prompt}] + messages[-3:]
+
+        chatbot_output = get_chatbot_response(self.client, self.model_name, input_messages)
+        output = self.postprocess(chatbot_output)
+
+        return output
+    
+    def get_response(self,messages):
+        messages = deepcopy(messages)
+
+        recommendation_classification = self.recommendation_classification(messages)
+        recommendation_type = recommendation_classification['recommendation_type']
+        recommendations = []
+        if recommendation_type == "apriori":
+            recommendations = self.get_apriori_recommendation(recommendation_classification['parameters'])
+        elif recommendation_type == "popular":
+            recommendations = self.get_popular_recommendation()
+        elif recommendation_type == "popular by category":
+            recommendations = self.get_popular_recommendation(recommendation_classification['parameters'])
+        
+        if recommendations == []:
+            return {"role": "assistant", "content":"Sorry, I can't help with that. Can I help you with your order?"}
+        
+        # Respond to User
+        recommendations_str = ", ".join(recommendations)
+        
+        system_prompt = f"""
+        You are a helpful AI assistant for a coffee shop application which serves drinks and pastries.
+        your task is to recommend items to the user based on their input message. And respond in a friendly but concise way. And put it an unordered list with a very small description.
+
+        I will provide what items you should recommend to the user based on their order in the user message. 
+        """
+
+        prompt = f"""
+        {messages[-1]['content']}
+
+        Please recommend me those items exactly: {recommendations_str}
+        """
+
+        messages[-1]['content'] = prompt
+        input_messages = [{'role': 'system', 'content': system_prompt}] + messages[-3:]
+
+        chatbot_output =get_chatbot_response(self.client, self.model_name, input_messages)
+        output = self.postprocess(chatbot_output)
+
+        return output
+    
+    def postprocess(self, output):
+        output = {
+            'role': 'assistant',
+            'content': output,
+            'memory': {'agent':'recommendation_agent'}
+        }
+        return output
+
+
