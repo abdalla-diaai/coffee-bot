07df597a9e0c1ddf6ff4ec52d95ba9f7b136d2c1
Abdalla Diaai <abdalla.diaai@outlook.com>
2025-01-27 18:16:58 +0000

    added tabs folder and home page.

--- coffee-shop-app/app/_layout.tsx
+++ coffee-shop-app/app/_layout.tsx
@@ -1,8 +1,21 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+
 // Import your global CSS file
 import "../global.css";
 
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontLoaded] = useFonts({
+    "Sora-Regular": require("../assets/fonts/Sora-Regular.ttf"),
+    "Sora-SemiBold": require("../assets/fonts/Sora-SemiBold.ttf"),
+    "Sora-Bold": require("../assets/fonts/Sora-Bold.ttf"),
+
+  })
+  return (
+  <Stack>  
+    <Stack.Screen name="index" options={{headerShown: false}} />
+    <Stack.Screen name="[tabs]" options={{headerShown: false}} /> 
+  </ Stack>
+);
 }
--- coffee-shop-app/app/index.tsx
+++ coffee-shop-app/app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { Text, View, SafeAreaView, ImageBackground, TouchableOpacity } from "react-native";
+import { router } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-red-600">Edit app/index.tsx to edit this screen.</Text>
+    <View>
+      <SafeAreaView className="w-full h-full">
+        <ImageBackground
+          className="w-full h-full items-center"
+          source={require("../assets/images/index_bg_image.png")}>
+          <View className="flex h-[60%]" />
+          <View className="flex w-[80%]">
+            <Text className="text-white text-3xl text-center mx-8 font-[Sora-SemiBold]">
+              Fall in Love with Coffee in a Blissful Delight!</Text>
+              <Text className="text-[#A2A2A2] text-center mx-8 font-[Sora-Regular] pt-3">
+              Fall in Love with Coffee in a Blissful Delight!</Text>
+              <TouchableOpacity 
+              className="bg-[#C67C4E] mt-10 py-3 rounded-lg items-center"
+              onPress={() => {router.push("/(tabs)/home")}}
+              >
+                <Text className="text-xl text-white font-[Sora-SemiBold]">Get Started</Text>
+              </TouchableOpacity>
+          </View>
+        </ImageBackground>
+      </SafeAreaView>
     </View>
   );
 }
